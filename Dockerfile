16a942de8a9557afa572d9ef7b30496e90d2ead5
Никита Быстров <hallnik96@gmail.com>
2022-12-22 19:53:25 +0300

    1.1 After first review

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.8.15-slim
-WORKDIR /app
-COPY . .
-RUN pip3 install -r api_yamdb/requirements.txt --no-cache-dir
-CMD ["gunicorn", "api_yamdb.api_yamdb.wsgi:application", "--bind", "0:8000" ]
--- api_yamdb/manage.py
+++ api_yamdb/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     os.environ.setdefault(
-        'DJANGO_SETTINGS_MODULE', 'api_yamdb.api_yamdb.settings'
+        'DJANGO_SETTINGS_MODULE', 'api_yamdb.settings'
     )
     try:
         from django.core.management import execute_from_command_line
