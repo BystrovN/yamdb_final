166d0d6abfe7dd6a3a7520b0bb3b550358032204
Никита Быстров <hallnik96@gmail.com>
2022-12-22 20:06:58 +0300

    1.1 After first review

--- api/views.py
+++ api/views.py
@@ -9,6 +9,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.permissions import AdminOrReadOnly, AuthorOrAdminOrReadOnly, IsAdmin
 
